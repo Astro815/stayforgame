dc6eb058eb31549653565e51a983ca147a4fee94
Astro_815 <astro815sfgKW@gmail.com>
2024-03-08 17:26:34 -0300

    hi

--- sfg_js/main.js
+++ sfg_js/main.js
@@ -3,7 +3,7 @@ const $ = (e) => { return document.querySelector(e); };
 let gameData;
 
 function loadGameData() {
-    fetch(`${window.location.origin}/sfg_data/game.json`)
+    fetch(`${window.location.origin}/stayforgame/sfg_data/game.json`)
         .then((data) => {
             data.json().then((json) => {
                 gameData = json;
